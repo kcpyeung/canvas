67ddad6f2ef5f882f16be722a06f6ae975369567
Kevin Yeung <github@yeung.online>
2017-09-01 23:52:17 +0800

    Refactoring: extract template function

--- src/canvas/line.clj
+++ src/canvas/line.clj
@@ -13,6 +13,13 @@
       (not (pos? y1))
       (> y2 max-y))))
 
+(defn- draw-line-on-canvas [canvas x1 y1 x2 y2 is-right-row? is-right-column?]
+  (if (out-of-bounds? canvas x1 y1 x2 y2)
+    nil
+    (letfn [(draw-columns [row] (map-indexed (fn [x-coord char-at-x-coord] (if (is-right-column? x-coord) "x" char-at-x-coord)) row))
+            (draw-rows [y-coord row] (if (is-right-row? y-coord) (draw-columns row) row))]
+      (map-indexed draw-rows canvas))))
+
 (defmulti line (fn [canvas x1 y1 x2 y2]
                  (cond
                    (= y1 y2) :horizontal
@@ -20,24 +27,15 @@
                    :default :diagonal)))
 
 (defmethod line :horizontal [canvas x1 y1 x2 y2]
-  (let [[x1' x2'] (sorted [x1 x2])]
-    (if (out-of-bounds? canvas x1' y1 x2' y2)
-      nil
-      (let [row (nth canvas (inc y1))
-            is-right-row? (partial = y1)
-            is-right-column? (set (range x1' (inc x2')))]
-        (letfn [(draw-columns [row] (map-indexed (fn [x-coord char-at-x-coord] (if (is-right-column? x-coord) "x" char-at-x-coord)) row))
-                (draw-rows [y-coord row] (if (is-right-row? y-coord) (draw-columns row) row))]
-          (map-indexed draw-rows canvas))))))
+  (let [[x1' x2'] (sorted [x1 x2])
+        is-right-row? (partial = y1)
+        is-right-column? (set (range x1' (inc x2')))]
+    (draw-line-on-canvas canvas x1' y1 x2' y2 is-right-row? is-right-column?)))
 
 (defmethod line :vertical [canvas x1 y1 x2 y2]
-  (let [[y1' y2'] (sorted [y1 y2])]
-    (if (out-of-bounds? canvas x1 y1' x2 y2')
-      nil
-      (let [is-right-row? (set (range y1' (inc y2')))
-            is-right-column? (partial = x1)]
-        (letfn [(draw-columns [row] (map-indexed (fn [x-coord char-at-x-coord] (if (is-right-column? x-coord) "x" char-at-x-coord)) row))
-                (draw-rows [y-coord row] (if (is-right-row? y-coord) (draw-columns row) row))]
-          (map-indexed draw-rows canvas))))))
+  (let [[y1' y2'] (sorted [y1 y2])
+        is-right-row? (set (range y1' (inc y2')))
+        is-right-column? (partial = x1)]
+    (draw-line-on-canvas canvas x1 y1' x2 y2' is-right-row? is-right-column?)))
 
 (defmethod line :diagonal [_ _ _ _ _] nil)
