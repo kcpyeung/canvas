5a338d25682fff38f93ba0516d11483831b1d758
Kevin Yeung <github@yeung.online>
2017-09-01 17:44:58 +0800

    Destruct x coordinates to avoid verbosity

--- src/canvas/line.clj
+++ src/canvas/line.clj
@@ -5,13 +5,13 @@
 
 (defn line [canvas x1 y1 x2 y2]
   (let [row (nth canvas (inc y1))
-        x1-x2 (sorted-x1-x2 [x1 x2])
-        should-draw-at? (set (range (first x1-x2) (inc (second x1-x2))))]
+        [x1 x2] (sorted-x1-x2 [x1 x2])
+        should-draw-at? (set (range x1 (inc x2)))]
     (letfn [(draw-line [x-coord] (if (should-draw-at? x-coord) "*" (nth row x-coord)))]
       (let [all-x-indexes (range (count row))
             drawn-line (map draw-line all-x-indexes)
             all-y-indexes (range (count canvas))]
         (letfn [(place-row-in-canvas [y-coord] (if (= y1 y-coord) drawn-line (nth canvas y-coord)))]
           (cond
-            (not (pos? (first x1-x2))) nil
+            (not (pos? x1)) nil
             :default (map place-row-in-canvas all-y-indexes)))))))
