d56d1ea50ab40385ae37f42a14d9b17dce380f8f
Kevin Yeung <github@yeung.online>
2017-09-01 14:30:14 +0800

    Draw horizontal line from right to left

--- src/canvas/line.clj
+++ src/canvas/line.clj
@@ -1,9 +1,13 @@
 (ns canvas.line)
 
+(defn- sorted-x1-x2 [[a b :as numbers]]
+  (if (> b a) numbers [b a]))
+
 (defn line [canvas x1 y1 x2 y2]
   (let [row (nth canvas (inc y1))
         all-char-indexes (range (count row))
-        line-char-position? (set (range x1 (inc x2)))]
+        x1-x2 (sorted-x1-x2 [x1 x2])
+        line-char-position? (set (range (first x1-x2) (inc (second x1-x2))))]
     (letfn [(draw-line [x-coord] (if (line-char-position? x-coord) "*" (nth row x-coord)))]
       (let [row-indexes (range (count canvas))
             drawn-line (map draw-line all-char-indexes)]
