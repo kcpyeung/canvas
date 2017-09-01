b7498235379289a83a9ecf3c549c71070dd755b2
Kevin Yeung <github@yeung.online>
2017-09-01 14:36:11 +0800

    Rename bindings

--- src/canvas/line.clj
+++ src/canvas/line.clj
@@ -5,11 +5,11 @@
 
 (defn line [canvas x1 y1 x2 y2]
   (let [row (nth canvas (inc y1))
-        all-char-indexes (range (count row))
+        all-x-indexes (range (count row))
         x1-x2 (sorted-x1-x2 [x1 x2])
-        line-char-position? (set (range (first x1-x2) (inc (second x1-x2))))]
-    (letfn [(draw-line [x-coord] (if (line-char-position? x-coord) "*" (nth row x-coord)))]
-      (let [row-indexes (range (count canvas))
-            drawn-line (map draw-line all-char-indexes)]
-        (letfn[(place-row-in-canvas [y-coord] (if (= y1 y-coord) drawn-line (nth canvas y-coord)))]
-          (map place-row-in-canvas row-indexes))))))
+        should-draw-at? (set (range (first x1-x2) (inc (second x1-x2))))]
+    (letfn [(draw-line [x-coord] (if (should-draw-at? x-coord) "*" (nth row x-coord)))]
+      (let [all-y-indexes (range (count canvas))
+            drawn-line (map draw-line all-x-indexes)]
+        (letfn [(place-row-in-canvas [y-coord] (if (= y1 y-coord) drawn-line (nth canvas y-coord)))]
+          (map place-row-in-canvas all-y-indexes))))))
