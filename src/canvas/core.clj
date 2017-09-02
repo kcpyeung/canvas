cf1af71f3dc51699573d73d9f2f05dc5c49f2889
Kevin Yeung <github@yeung.online>
2017-09-02 11:13:40 +0800

    Add skeletal main loop to accept user inputs - use 'lein run' to invoke

--- src/canvas/core.clj
+++ src/canvas/core.clj
@@ -1,3 +1,13 @@
 (ns canvas.core
   (:require [canvas.canvas :refer :all]
-            [canvas.line :refer :all]))
+            [canvas.line :refer :all]
+            [clojure.string :refer [starts-with?]])
+  (:gen-class :name canvas.core :main true))
+
+(defn -main []
+  (loop [line ""]
+    (cond
+      (starts-with? line "Q") (System/exit 0)
+      (starts-with? line "C") (println "Canvas!")
+      (starts-with? line "L") (println "Line!"))
+    (recur (read-line))))
