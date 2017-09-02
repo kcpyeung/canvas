a0bcaede36ccdc60013f058ba93b012c0586a84c
Kevin Yeung <github@yeung.online>
2017-09-02 12:01:11 +0800

    Main loop to collect commands and print canvas

--- src/canvas/core.clj
+++ src/canvas/core.clj
@@ -1,13 +1,26 @@
 (ns canvas.core
   (:require [canvas.canvas :refer :all]
             [canvas.line :refer :all]
-            [clojure.string :refer [starts-with?]])
+            [clojure.string :refer [join]])
   (:gen-class :name canvas.core :main true))
 
+(def current-canvas (atom nil))
+
+(defn- draw [rows]
+  (if rows
+    (->> rows
+         (map (fn [row] (apply str row)))
+         (join \newline)
+         println)))
+
 (defn -main []
-  (loop [line ""]
-    (cond
-      (starts-with? line "Q") (System/exit 0)
-      (starts-with? line "C") (println "Canvas!")
-      (starts-with? line "L") (println "Line!"))
-    (recur (read-line))))
+  (loop [[command & [_ & rest]] ""]
+    (let [args (join rest)]
+      (cond
+        (= \Q command) (System/exit 0)
+        (= \C command) (swap! current-canvas (fn [_] (canvas 10 8)))
+        (= \L command) (swap! current-canvas (fn [latest] (line latest 3 5 7 5))))
+      (draw @current-canvas)
+      (print "enter command: ")
+      (flush)
+      (recur (read-line)))))
