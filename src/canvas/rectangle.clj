d9556b15b007eba4337225483035e31ba0ff0a86
Kevin Yeung <github@yeung.online>
2019-01-03 20:45:00 +1100

    Remove nil-check and simplify rectangle function

--- src/canvas/rectangle.clj
+++ src/canvas/rectangle.clj
@@ -9,10 +9,8 @@
     (list x1 y2 x1 y1)))
 
 (defn- draw-line [canvas [x1 y1 x2 y2]]
-  (let [new-canvas (line canvas x1 y1 x2 y2)]
-    (if (= canvas new-canvas) nil new-canvas)))
+  (line canvas x1 y1 x2 y2))
 
 (defn rectangle [canvas x1 y1 x2 y2]
-  (let [lines (line-coords x1 y1 x2 y2)
-        new-canvas (reduce draw-line canvas lines)]
-    (if (nil? new-canvas) canvas new-canvas)))
+  (->> (line-coords x1 y1 x2 y2)
+       (reduce draw-line canvas)))
