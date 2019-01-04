14bbb0a344c244d2fbca35343ae17f152c131362
Kevin Yeung <github@yeung.online>
2019-01-04 16:56:31 +1100

    Remove the use of atom

--- src/canvas/core.clj
+++ src/canvas/core.clj
@@ -5,8 +5,6 @@
             [clojure.string :refer [join split]])
   (:gen-class :name canvas.core :main true))
 
-(def current-canvas (atom nil))
-
 (defn- split-args [args]
   (->> (split args #" ")
        (map #(Integer/parseInt %1))))
@@ -19,14 +17,15 @@
          println)))
 
 (defn -main []
-  (loop [[command & [_ & rest]] ""]
-    (let [args (join rest)]
-      (cond
-        (= \Q command) (System/exit 0)
-        (= \C command) (let [[x y] (split-args args)] (swap! current-canvas (fn [_] (canvas x y))))
-        (= \L command) (let [[x1 y1 x2 y2] (split-args args)] (swap! current-canvas (fn [latest] (line latest x1 y1 x2 y2))))
-        (= \R command) (let [[x1 y1 x2 y2] (split-args args)] (swap! current-canvas (fn [latest] (rectangle latest x1 y1 x2 y2)))))
-      (draw @current-canvas)
+  (loop [[command & [_ & rest]] ""
+         current-canvas         nil]
+    (let [args    (join rest)
+          to-draw (cond
+                    (= \Q command) (System/exit 0)
+                    (= \C command) (let [[x y] (split-args args)] (canvas x y))
+                    (= \L command) (let [[x1 y1 x2 y2] (split-args args)] (line current-canvas x1 y1 x2 y2))
+                    (= \R command) (let [[x1 y1 x2 y2] (split-args args)] (rectangle current-canvas x1 y1 x2 y2)))]
+      (draw to-draw)
       (print "enter command: ")
       (flush)
-      (recur (read-line)))))
+      (recur (read-line) to-draw))))
