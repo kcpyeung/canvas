67c12a83089f9f96e4ac2ef26eb5c0048e24c013
Kevin Yeung <github@yeung.online>
2019-02-01 19:44:47 +1100

    Add ability to draw around borders

--- src/canvas/bucket_fill.clj
+++ src/canvas/bucket_fill.clj
@@ -3,7 +3,10 @@
     [canvas.point :refer :all]))
 
 (defn bucket-fill [canvas char x y]
-  (let [neighbours (neighbours canvas x y)
-        drawn      (->> neighbours
-                        (reduce #(point %1 char (first %2) (second %2)) canvas))]
-    (reduce #(bucket-fill %1 char (first %2) (second %2)) drawn neighbours)))
+  (if (or (is-empty? canvas [x y]) (= char (at canvas x y)))
+    (let [neighbours (neighbours canvas x y)
+          drawn      (->> neighbours
+                          (cons [x y])
+                          (reduce #(point %1 char (first %2) (second %2)) canvas))]
+      (reduce #(bucket-fill %1 char (first %2) (second %2)) drawn neighbours))
+    canvas))
--- test/canvas/bucket_fill_test.clj
+++ test/canvas/bucket_fill_test.clj
@@ -1,7 +1,7 @@
 (ns canvas.bucket-fill-test
   (:require [clojure.test :refer :all]
             [canvas.canvas :refer :all]
-            [canvas.point :refer :all]
+            [canvas.line :refer :all]
             [canvas.rectangle :refer :all]
             [canvas.bucket-fill :refer :all]))
 
@@ -21,7 +21,53 @@
                    (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
                    (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
                    (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
-                   (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-")) (bucket-fill canvas "o" 4 4))))))
+                   (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-")) (bucket-fill canvas "o" 4 4)))))
+
+  (deftest filling-canvas-with-objects
+    (testing "filling inside"
+             (is
+              (= (list
+                  (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-")
+                  (list "|" " " " " " " " " " " " " " " " " "|")
+                  (list "|" " " "x" "x" "x" "x" "x" " " " " "|")
+                  (list "|" " " "x" "o" "o" "o" "x" " " " " "|")
+                  (list "|" " " "x" "o" "o" "o" "x" " " " " "|")
+                  (list "|" " " "x" "o" "o" "o" "x" " " " " "|")
+                  (list "|" " " "x" "x" "x" "x" "x" " " " " "|")
+                  (list "|" " " " " " " " " " " " " " " " " "|")
+                  (list "|" " " " " " " " " " " " " " " " " "|")
+                  (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-")) (bucket-fill canvas-with-rectange "o" 4 4))))
+
+    (testing "filling outside"
+             (is
+              (= (list
+                  (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-")
+                  (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                  (list "|" "o" "x" "x" "x" "x" "x" "o" "o" "|")
+                  (list "|" "o" "x" " " " " " " "x" "o" "o" "|")
+                  (list "|" "o" "x" " " " " " " "x" "o" "o" "|")
+                  (list "|" "o" "x" " " " " " " "x" "o" "o" "|")
+                  (list "|" "o" "x" "x" "x" "x" "x" "o" "o" "|")
+                  (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                  (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                  (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-")) (bucket-fill canvas-with-rectange "o" 1 1))))
+
+    (testing "filling around line"
+             (let [canvas-with-line (line canvas 2 2 6 2)]
+               (is
+                (=
+                  (list
+                   (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-")
+                   (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                   (list "|" "o" "x" "x" "x" "x" "x" "o" "o" "|")
+                   (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                   (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                   (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                   (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                   (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                   (list "|" "o" "o" "o" "o" "o" "o" "o" "o" "|")
+                   (list "-" "-" "-" "-" "-" "-" "-" "-" "-" "-"))
+                  (bucket-fill canvas-with-line "o" 1 1)))))))
 
 
 (run-tests)
