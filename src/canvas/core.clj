79adfe1be4858bbf7f059f1635afb99bd50906fa
Kevin Yeung <github@yeung.online>
2019-02-04 11:11:48 +1100

    Expose bucket fill to interface

--- src/canvas/core.clj
+++ src/canvas/core.clj
@@ -2,6 +2,7 @@
   (:require [canvas.canvas :refer :all]
             [canvas.line :refer :all]
             [canvas.rectangle :refer :all]
+            [canvas.bucket-fill :refer :all]
             [clojure.string :refer [join split]])
   (:gen-class :name canvas.core :main true))
 
@@ -24,7 +25,8 @@
                     (= \Q command) (System/exit 0)
                     (= \C command) (let [[x y] (split-args #(Integer/parseInt %) args)] (canvas x y))
                     (= \L command) (let [[x1 y1 x2 y2] (split-args #(Integer/parseInt %) args)] (line current-canvas x1 y1 x2 y2))
-                    (= \R command) (let [[x1 y1 x2 y2] (split-args #(Integer/parseInt %) args)] (rectangle current-canvas x1 y1 x2 y2)))]
+                    (= \R command) (let [[x1 y1 x2 y2] (split-args #(Integer/parseInt %) args)] (rectangle current-canvas x1 y1 x2 y2))
+                    (= \F command) (let [[char x y] (split-args identity args)] (bucket-fill current-canvas char (Integer/parseInt x) (Integer/parseInt y))))]
       (draw to-draw)
       (print "enter command: ")
       (flush)
